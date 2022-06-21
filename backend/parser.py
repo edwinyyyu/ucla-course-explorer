4a999616259e36c28876ff58594f44a41395b037
Edwin Yu <edwinyyyu@gmail.com>
2022-06-20 17:17:43 -0700

    Add basic parse functionality

--- backend/parser.py
+++ backend/parser.py
@@ -6,44 +6,36 @@ def load_courses(lines, start, end):
             course_id = lines[i - 1].strip()
 
 def parse_data(file):
-    lines = file.readlines()
+    lines = [line.strip() for line in file.readlines()]
+    course_subject = lines[0].strip()
+    course_level = ""
     
-    lower_div_mark = -1
-    upper_div_mark = -1
-    graduate_mark = -1
     for i in range(len(lines)):
         if lines[i].startswith("Lower Division Courses"):
-            lower_div_mark = i
+            course_level = "lower division"
             continue
-        if lines[i].startswith("Upper Division Courses"):
-            upper_div_mark = i
+        elif lines[i].startswith("Upper Division Courses"):
+            course_level = "upper division"
             continue
-        if lines[i].startswith("Graduate Courses"):
-            graduate_mark = i
+        elif lines[i].startswith("Graduate Courses"):
+            course_level = "graduate division"
             continue
     
-    course_level = "lower division"
-    for i in range(lower_div_mark + 1, upper_div_mark):
         if lines[i].startswith("Units: "):
-            course_id = lines[i - 1]
-    
-    course_level = "upper division"
-    for i in range(upper_div_mark + 1, graduate_mark):
-        if lines[i].startswith("Units: "):
-            course_id = lines[i - 1]
-    
-    course_level = "graduate"
-    for i in range(graduate_mark + 1, len(lines)):
-        if lines[i].startswith("Units: "):
-            course_id = lines[i - 1]
-    
+            course_number = lines[i - 1][ : lines[i - 1].find(".")]
+            course_name = lines[i - 1][lines[i - 1].find(" ") + 1 : ]
+            num_units = lines[i][lines[i].find(" ") + 1 : ]
+            
+            print(course_number)
+            print(course_name)
+            print(num_units)
 
 # main
 directory = "course-descriptions-data"
 
-for filename in os.listdir(directory):
-    with open(directory + "/" + filename, "r") as f:
-        parse_data(f)
+#for filename in os.listdir(directory):
+#    with open(directory + "/" + filename, "r") as f:
+#        parse_data(f)
 
 with open("course-descriptions-data/computer_science.txt", "r") as f:
     parse_data(f)
