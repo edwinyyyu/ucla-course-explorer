b0f2b67b5ff3300bcc7c2fc461cb3650a50689f4
Edwin Yu <edwinyyyu@gmail.com>
2023-01-03 20:16:03 -0800

    Add GET methods for MongoDB documents to Express app

--- backend/server/routes/courses.js
+++ backend/server/routes/courses.js
@@ -6,28 +6,65 @@ const { getDb } = require('../db');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('courses');
-});
-
-router.get('/:subject/:number', (req, res) => {
   getDb().collection('courses')
-  .findOne({
-    subject: req.params.subject.replaceAll('_', ' '),
-    number: req.params.number
-  })
-  .then(doc => {res.status(200).json(doc)})
-  .catch(err => {res.status(500).json({ error: 'Could not fetch doc' })});
+  .find()
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
 });
 
-router.get('/:id', (req, res) => {
+router.get('/id/:id', (req, res) => {
   if (ObjectId.isValid(req.params.id)) {
     getDb().collection('courses')
-    .findOne({ _id: ObjectId(req.params.id) })
+    .findOne({
+      _id: ObjectId(req.params.id)
+    })
     .then(doc => {res.status(200).json(doc)})
     .catch(err => {res.status(500).json({ error: 'Could not fetch doc' })});
   } else {
     res.status(500).json({ error: 'Invalid doc id' });
   }
+});
+
+router.get('/level/:level', (req, res) => {
+  getDb().collection('courses')
+  .find({
+    level: req.params.level.replaceAll('_', ' ')
+  })
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
+});
+
+router.get('/subject/:subject', (req, res) => {
+  getDb().collection('courses')
+  .find({
+    subject: req.params.subject.replaceAll('_', ' ')
+  })
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
+});
+
+router.get('/subject/:subject/level/:level', (req, res) => {
+  getDb().collection('courses')
+  .find({
+    subject: req.params.subject.replaceAll('_', ' '),
+    level: req.params.level.replaceAll('_', ' ')
+  })
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
+});
+
+router.get('/subject/:subject/number/:number', (req, res) => {
+  getDb().collection('courses')
+    .findOne({
+      subject: req.params.subject.replaceAll('_', ' '),
+      number: req.params.number
+    })
+    .then(doc => {res.status(200).json(doc)})
+    .catch(err => {res.status(500).json({ error: 'Could not fetch doc' })});
 });
 
 module.exports = router;
--- backend/server/routes/requisites.js
+++ backend/server/routes/requisites.js
@@ -5,21 +5,67 @@ const { getDb } = require('../db');
 
 const router = express.Router();
 
-const db = getDb();
-
 router.get('/', (req, res) => {
-  res.send('requisites');
+  getDb().collection('requisites')
+  .find()
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
 });
 
-router.get('/:id', (req, res) => {
+router.get('/id/:id', (req, res) => {
   if (ObjectId.isValid(req.params.id)) {
     getDb().collection('requisites')
-    .findOne({ _id: ObjectId(req.params.id) })
+    .findOne({
+      _id: ObjectId(req.params.id)
+    })
     .then(doc => {res.status(200).json(doc)})
     .catch(err => {res.status(500).json({ error: 'Could not fetch doc' })});
   } else {
     res.status(500).json({ error: 'Invalid doc id' });
   }
+});
+
+router.get('/source/subject/:subject', (req, res) => {
+  getDb().collection('requisites')
+  .find({
+    source_subject: req.params.subject.replaceAll('_', ' ')
+  })
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
+});
+
+router.get('/source/subject/:subject/number/:number', (req, res) => {
+  getDb().collection('requisites')
+  .find({
+    source_subject: req.params.subject.replaceAll('_', ' '),
+    source_number: req.params.number
+  })
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
+});
+
+router.get('/target/subject/:subject', (req, res) => {
+  getDb().collection('requisites')
+  .find({
+    target_subject: req.params.subject.replaceAll('_', ' ')
+  })
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
+});
+
+router.get('/target/subject/:subject/number/:number', (req, res) => {
+  getDb().collection('requisites')
+  .find({
+    target_subject: req.params.subject.replaceAll('_', ' '),
+    target_number: req.params.number
+  })
+  .toArray()
+  .then(docs => {res.status(200).json(docs)})
+  .catch(err => {res.status(500).json({ error: 'Could not fetch docs' })});
 });
 
 module.exports = router;
